af45b73dd06606149465a274cc45198e957d109c
Oreo0xx7 <95393325+Oreo0xx7@users.noreply.github.com>
2023-06-14 03:23:29 +0900

    Update help_notice.dart

--- lib/pages/help_notice.dart
+++ lib/pages/help_notice.dart
@@ -25,9 +25,47 @@ class _HelpNotice extends StatelessWidget {
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.all(1)),
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.maxFinite,
+              height: 70,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(border: Border.all(color: Colors.black38)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text("서비스 이용약관 개정 안내", style: TextStyle(color: Colors.black, fontSize: 13),),
+                Text("2023-05-23 13:13", style: TextStyle(color: Colors.black26, fontSize: 11),)],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.maxFinite,
+              height: 70,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(border: Border.all(color: Colors.black38)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text("북맵 서비스 이용 안내", style: TextStyle(color: Colors.black, fontSize: 13),),
+                  Text("2023-04-19 18:10", style: TextStyle(color: Colors.black26, fontSize: 11),)],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.maxFinite,
+              height: 70,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(border: Border.all(color: Colors.black38)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text("서비스 이용약관 개정 안내", style: TextStyle(color: Colors.black, fontSize: 13),),
+                  Text("2023-03-21 07:19", style: TextStyle(color: Colors.black26, fontSize: 11),)],
+              ),
+            )
+          ],
         ),
       ),
     );
   }
-
 }
